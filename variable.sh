8ac6e1cd84f3bfccd11086454fc52a50dbb53326
Bral <VieuxLoupGris>
2019-02-20 09:35:25 +0100

    Initial commit

---
+++ variable.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#DDeclare executable variable with ``.
+
+message=`pwd`
+
+
+#Variable is executed.
+
+echo "You are in the repertory $message"
+
+
+hello='Hello every one'
+
+#Show variable content.
+
+echo "The message is: $hello"
+
+
+#Show $hello
+
+echo 'The message is: $hello'
+
