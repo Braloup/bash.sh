f735a1544d285c73462174f2f3eee7d3f41344c7
Bral <VieuxLoupGris>
2019-02-20 09:50:18 +0100

    Create multiple variable with read

---
+++ read.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo 'Enter your name:'
+
+#read wait the user input
+
+read firstName
+
+echo "Hello $firstName !"
+
+#read can accept multiple parametre
+
+echo 'Ente first name and lastname'
+
+read firstName lastName
+
+echo "Hello $firstName $lastName"
